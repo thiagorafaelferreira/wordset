4329ab0d3aa5aa572c7e6f6229f7120e5534654e
Thiago Rafael Ferreira <thiagorafaelferreira@gmail.com>
2022-04-11 13:43:39 -0300

    Update WordsController.java

--- src/main/java/com/ferreiracodando/wordle/controller/WordsController.java
+++ src/main/java/com/ferreiracodando/wordle/controller/WordsController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -15,7 +14,6 @@ import java.io.IOException;
 import java.util.Set;
 
 @RestController
-@RequestMapping("/v1")
 @AllArgsConstructor
 @Validated
 public class WordsController {
