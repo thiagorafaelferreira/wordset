ad27591422639f9a2ddb107bca2d050b302c43ff
Thiago Rafael Ferreira <thiagorafaelferreira@gmail.com>
2022-04-11 12:16:28 -0300

    configured heroku sdk

--- src/main/java/com/ferreiracodando/wordle/controller/WordsController.java
+++ src/main/java/com/ferreiracodando/wordle/controller/WordsController.java
@@ -3,20 +3,31 @@ package com.ferreiracodando.wordle.controller;
 import com.ferreiracodando.wordle.service.WordsService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Max;
 import java.io.IOException;
 import java.util.Set;
 
 @RestController
 @RequestMapping("/v1/words")
 @AllArgsConstructor
+@Validated
 public class WordsController {
 
     private final WordsService wordsService;
+
+    @GetMapping("/length/{language}")
+    public ResponseEntity<String> word(@PathVariable("language") @Valid @Max(3) Integer language) throws IOException {
+        return ResponseEntity
+                .ok()
+                .body(wordsService.randomWord(language.toString()));
+    }
 
     @GetMapping("/random/{language}")
     public ResponseEntity<String> random(@PathVariable("language") String language) throws IOException {
